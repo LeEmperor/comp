6b5079960eccbf64b8736bbd45facc200c1d6244
LeEmperor <101953197+LeEmperor@users.noreply.github.com>
2022-10-28 05:05:05 -0400

    Update d.cpp

--- 1742-D4/d.cpp
+++ 1742-D4/d.cpp
@@ -20,8 +20,39 @@ typedef vector<VS> VSS;
 typedef pair<int, int> PII;
 typedef pair<int, string> PIS;
 
-string solve(int i) {
-	return "bruh";
+VI factorSets(int input) {
+    VI output = {1, input};
+    if(input == 2 || input == 3) {
+        return output;
+    }
+
+    if(input % 2 == 0) {
+        // even
+        output.push_back(input / 2);
+        output.push_back(2);
+        return output;
+    }
+
+    // need a list of primes
+    VI primes;
+
+    for(int i : primes) {
+        if(input % i == 0) {
+            output.push_back(i);
+            output.push_back(input / i);
+        }
+    }
+
+}
+
+int solve(VI input) {
+    int a = 0;
+
+    VVI factorizaiton;
+
+    
+    
+    return a;
 }
 
 int main() {
@@ -34,8 +65,15 @@ int main() {
 	while(testcases--) {
 		int n;
 		cin >> n;
-		
-		cout << solve(n) << nL;
+
+        VI input;
+        while(n--) {
+            int i;
+            cin >> i;
+            input.push_back(i);
+        }
+
+        cout << solve(input) << nL;
 	}	
 }
 
