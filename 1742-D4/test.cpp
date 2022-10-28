c21159240f05a79ee9c970641f23dd9014f01d07
LeEmperor <bohdanpurtell@gmail.com>
2022-10-28 06:39:23 -0400

    1742
    First attempt at problem d

--- 1742-D4/test.cpp
+++ 1742-D4/test.cpp
@@ -20,13 +20,10 @@ typedef vector<VS> VSS;
 typedef pair<int, int> PII;
 typedef pair<int, string> PIS;
 
-int GCD(int ina, int inb) {
-    // a = 1071
-    // b = 462
-
+int solve(int ai, int bi) {
     int t = 0;
-    int b = inb;
-    int a = ina;
+    int b = bi;
+    int a = ai;
 
     while(b != 0) {
         t = b;
@@ -35,26 +32,6 @@ int GCD(int ina, int inb) {
     }
 
     return a;
-
-}
-
-int solve(VI input) {
-
-    VI bruh;
-
-    for(int i = 0; i < input.size(); i++) {
-        for(int j = 0; j < input.size(); j++) {
-            if(GCD(input[i], input[j]) == 1) {
-                bruh.push_back(i + j);
-            }
-        }
-    }
-
-    if(bruh.begin() == bruh.end()) {
-        return -1;
-    }
-
-    return *max_element(all(bruh)) + 2;
 }
 
 int main() {
@@ -65,17 +42,9 @@ int main() {
 	cin >> testcases;
 
 	while(testcases--) {
-		int n;
-		cin >> n;
-
-        VI input;
-        while(n--) {
-            int bruh2;
-            cin >> bruh2;
-            input.push_back(bruh2);
-        }
-
-        cout << solve(input) << nL;
+        int a, b;
+        cin >> a >> b;
+        cout << solve(a, b) << nL;
 	}	
 }
 
