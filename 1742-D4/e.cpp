d126928a9408f23752d0b46beb2576a886493fc7
Bohdan Purtell <bohdanpurtell@gmail.com>
2022-11-29 15:35:11 -0500

    merging comp trees from Linux systems

--- 1742-D4/e.cpp
+++ 1742-D4/e.cpp
@@ -4,7 +4,6 @@
 #include <bitset> 
 #include <cmath> 
 #include <algorithm> 
-#include <set>
 using namespace std;
 
 #define nL "\n"
@@ -15,52 +14,43 @@ typedef vector<int> VI;
 typedef vector<VI> VVI;
 typedef long long ll;
 typedef vector<ll> Vll;
-typedef vector<string> VS;
-typedef vector<VS> VSS;
-typedef pair<int, int> PII;
-typedef pair<int, string> PIS;
 
-int BS(ll bruh, vector<long long> monotonic, ll LB, ll UB) {
+int last_true(vector<long long> data, ll findThis) {
+	int low = 0;
+	int high = data.size() - 1;
 
-        ll M = ((UB - LB + 1) / 2) + LB;
-        if(UB == LB) {
-                return UB;
-        }
+	while(low < high) {
+		int mid = low + (high - low + 1) / 2;
 
-        if(bruh >=  monotonic[M]) {
-                // in the upper half, LB becomes M
-                return BS(bruh, monotonic, M, UB);
-                
-        } else if(bruh < monotonic[M]) {
-                // in the lower half, UB becomes M
-                // the step is too large
-                return BS(bruh, monotonic, LB, M-1);
-        }
+		if(data[mid] > findThis) {
+			high = mid - 1;
+		} else if(data[mid] == findThis) {
+			low = mid;
+		} else {
+			// data[mid] < findThis
+			low = mid;
+		}
 
-        cout << "we are at the end for some reason" << nL;
-        return -1;
+
+	}
+
+	return low + 1;
 }
 
-void wombo_combo(vector<ll> steps, vector<ll> questions) {
-        vector<long long> maxSeen(steps.size(), 0);
-        maxSeen[0] = steps[0];
-        for(int i = 1; i < steps.size(); i++) {
-                maxSeen[i] = max(steps[i], maxSeen[i-1]);
-        }
+Vll solve(vector<ll> input, vector<ll> questions) {
+	Vll maxSeen(input.size(), 0);
+	maxSeen[0] = input[0];
+	Vll ans(input.size(), 0);
 
-        for(ll i = 0; i < questions.size(); i++) {
-                ll ans = 0;
-                ll stoppingPoint = BS(questions[i], maxSeen, -1, steps.size()-1);
-                // being very fucky when the k is less than all of the things
+	for(int i = 1; i < input.size(); i++) {
+		maxSeen[i] = max(maxSeen[i], input[i-1]);
+	}
 
-                for(long long j = 0; j < stoppingPoint + 1; j++) {
-                        ans += steps[j];
-                }
+	for(int i = 0; i < questions.size(); i++) {
+		ans[i] = input[last_true(maxSeen, questions[i])];
+	}
 
-                cout << ans << " ";
-        }
-        cout << nL;
-        
+	return ans;
 }
 
 int main() {
@@ -71,24 +61,32 @@ int main() {
 	cin >> testcases;
 
 	while(testcases--) {
-		ll n, q;
+		int n, q;
 		cin >> n >> q;
-		
-                vector<ll> steps;
-                vector<ll> questions;
-                while(n--) {
-                        ll i;
-                        cin >> i;
-                        steps.push_back(i);
-                }
 
-                while(q--) {
-                        ll j;
-                        cin >> j;
-                        questions.push_back(j);
-                }
+		Vll raw;
+		Vll questions;
 
-                wombo_combo(steps, questions);
-	}	
+		while(n--) {
+			ll i;
+			cin >> i;
+			raw.push_back(i);
+		}
+
+		while(q--) {
+			ll j;
+			cin >> j;
+			questions.push_back(j);
+		}
+
+		Vll ans = solve(raw, questions);
+
+		for(ll i : ans) {
+			cout << i << " ";
+		}
+
+		cout << nL;
+
+	}
 }
 
